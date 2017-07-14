22a5f91d0b8c68a1e71dad2067de122ad8d0b846
Wes Cole <wesley.cole87@gmail.com>
2017-07-14 14:21:00 -0400

    first commit

---
+++ simplyrets-history.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SimplyRETS History
+Description: Display pricing history for specific properties
+Version: 1.0.0
+Author: Wesley Cole <wes@objectiv.co>
+*/
+if ( ! defined( 'WPINC' ) )
+    die;
+
+defined( "RETS_HISTORY_URI" ) or define( "RETS_HISTORY_URI", plugin_dir_url( __FILE__ ) );
